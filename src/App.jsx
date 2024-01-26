5029d599e211ab8c3e81e1e78c0077934c927924
kvdcre8r <kvdcre8r@gmail.com>
2024-01-25 19:35:09 -0600

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import {puppyList} from './data.js'
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+
+  function handleClick(puppy) {
+    console.log("puppy id: ", puppy.id);
+    setFeatPupId(puppy.id);
+  }
   return (
-    <div></div>
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => handleClick(puppy)} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      {featPupId && <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>}
+    </div>
   )
 }
 
